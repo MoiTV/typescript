5172c0689f6e4d3e5904e81486410bf549058f25
Moises Valdez <moisesvaldez075@gmail.com>
2020-04-11 13:23:44 -0700

    first time using typescript

---
+++ main.ts
@@ -0,0 +1,24 @@
+// Boolean
+let isCool: boolean = true;
+
+// Number
+let age: number = 50;
+
+// String
+let eye: string = 'round';
+let favQuote: string = `I'm not old, i'm only ${age}`;
+
+// Array
+let pets: string[] = ['cat', 'dog', 'pig'];
+
+let pets2: Array<string> = ['li'];
+
+// Object
+let wizard: object = {
+    a: 'moises'
+};
+
+// null and undefined
+let meh: undefined = undefined;
+
+let no: null = null;
