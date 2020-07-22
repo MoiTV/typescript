6f02279d9007329d6999396b2ebb2020aed726ab
Moises Valdez <moisesvaldez075@gmail.com>
2020-07-22 09:01:10 -0700

    reviewing arrays methods

---
+++ app.js
@@ -0,0 +1,18 @@
+let arr1 = [3, 2, 1];
+let arr2 = [4, 5, 6, 1];
+
+function hashMap(arr1, arr2) {
+    // for (let i = 0; i < arr1.length; i++) {
+    //     for (let j = 0; j < arr2.length; j++) {
+    //         if (arr1[i] === arr2[j]) {
+    //             return console.log(arr1[i]);
+    //         }
+    //     }
+    // }
+
+    let map = {};
+
+    return undefined;
+}
+
+console.log(hashMap(arr1, arr2));
