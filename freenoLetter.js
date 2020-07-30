463027441b73f8f3191da4749dec06153939ab51
Moises Valdez <moisesvaldez075@gmail.com>
2020-07-30 15:36:48 -0700

    a fibonacci number that will sum all the odd number

---
+++ freenoLetter.js
@@ -0,0 +1,19 @@
+function fearNotLetter(str) {
+    // we can skip the 1st letter
+    for (let i = 1; i < str.length; i++) {
+        // get the char code of the previous letter
+        const prev = str[i - 1].charCodeAt();
+        // get the char code of the current letter
+        const current = str[i].charCodeAt();
+
+        if (current - prev !== 1) {
+            // if the difference between current and previous is not 1
+            // get the character after the previous
+            return String.fromCharCode(prev + 1);
+        }
+    }
+
+    return null; // if nothing is found
+}
+
+console.log(fearNotLetter('abce'));
--- main.js
+++ main.js
@@ -1,23 +1,18 @@
-const log = console.log;
+function sumFibs(num) {
+    let prevNum = 0;
+    let curNum = 1;
+    let result = 0;
 
-function uniteUnique(arr) {
-    let finalArray = [];
-
-    for (let i = 0; i < arguments.length; i++) {
-        let arrayArguments = arguments[i];
-        log(arrayArguments);
-        // Loops through the array at hand
-        for (let j = 0; j < arrayArguments.length; j++) {
-            let indexValue = arrayArguments[j];
-
-            // Checks if the value is already on the final array
-            if (finalArray.indexOf(indexValue) < 0) {
-                finalArray.push(indexValue);
-            }
+    while (curNum <= num) {
+        if (curNum % 2 !== 0) {
+            result += curNum;
         }
+
+        curNum += prevNum;
+        prevNum = curNum - prevNum;
     }
 
-    log(finalArray);
+    return result;
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+console.log(sumFibs(10));
