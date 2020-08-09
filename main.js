2d2e6e1a028c3d64a19cc9f61bb11d6e41c7e572
Moises Valdez <moisesvaldez075@gmail.com>
2020-08-09 13:48:35 -0700

    working with hash tables

--- main.js
+++ main.js
@@ -1,18 +1,4 @@
-function sumFibs(num) {
-    let prevNum = 0;
-    let curNum = 1;
-    let result = 0;
-
-    while (curNum <= num) {
-        if (curNum % 2 !== 0) {
-            result += curNum;
-        }
-
-        curNum += prevNum;
-        prevNum = curNum - prevNum;
-    }
-
-    return result;
-}
-
-console.log(sumFibs(10));
+// Hash Table
+const hash = {
+    key: 'value'
+};
