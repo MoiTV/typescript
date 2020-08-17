45170609c045315f49bd03287de138b4d8d3508a
Moises Valdez <moisesvaldez075@gmail.com>
2020-08-16 23:34:42 -0700

    added objects

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void menu();
+
+int main()
+{
+    menu();
+
+    return (0);
+}
+
+void menu()
+{
+    int selectedNum;
+
+    cout << "* Select a Number * " << endl;
+    cout << "* 1.  * " << endl;
+    cout << "* 2.  * " << endl;
+    cout << "* 3.  * " << endl;
+    cout << "* 4.  * " << endl;
+}
